24a5a89a3c0bb216fa3c8f133f3fb01c8a95d103
Nix <git@starnix.dev>
2026-06-11 20:01:26 -0400

    Add support for multiple callback URLs

--- main.go
+++ main.go
@@ -2,45 +2,48 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
+	"strings"
 
-    "net/http"
-    "net/url"
-    
-    "bytes"
-    "encoding/json"
+	"net/http"
+	"net/url"
 
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/core"
+	"bytes"
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    callback_url := os.Getenv("CMS_CALLBACK_URL")
-    if (len(callback_url) > 0) {
-        _, err := url.ParseRequestURI(callback_url)
-        if err != nil {
-            panic(err)
-        }
+	callback_url := os.Getenv("CMS_CALLBACK_URL")
+	if len(callback_url) > 0 {
+		for _, v := range strings.Split(callback_url, ";") {
+			_, err := url.ParseRequestURI(v)
+			if err != nil {
+				panic(err)
+			}
 
-        handleModelUpdate := func(e *core.ModelEvent) error {
-            json, err := json.Marshal(e.Model)
-            jsonBody := bytes.NewBuffer(json)
-            _, err = http.Post(callback_url, "application/json", jsonBody)
-            if err != nil {
-                log.Fatalln(err)
-            }
-            return nil
-        }
-        
-        app.OnModelAfterCreate().Add(handleModelUpdate)
-        app.OnModelAfterUpdate().Add(handleModelUpdate)
-        app.OnModelAfterDelete().Add(handleModelUpdate)
-    }
+			handleModelUpdate := func(e *core.ModelEvent) error {
+				json, err := json.Marshal(e.Model)
+				jsonBody := bytes.NewBuffer(json)
+				_, err = http.Post(v, "application/json", jsonBody)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				return nil
+			}
 
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+			app.OnModelAfterCreate().Add(handleModelUpdate)
+			app.OnModelAfterUpdate().Add(handleModelUpdate)
+			app.OnModelAfterDelete().Add(handleModelUpdate)
+		}
+	}
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
